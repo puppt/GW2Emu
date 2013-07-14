de9b2eba328b1920f204c2be294b652720e5edcc
ephe-meral <j.appel.online@gmail.com>
2013-07-14 15:11:38 +0200

    changed gitignore to drop OSX and Mono stuff

---
+++ GameRevision.GW2Emu.Tests/ClientManagerTest.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace GameRevision.GW2Emu.Network
+{
+	[TestFixture()]
+	public class ClientManagerTest
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
---
+++ GameRevision.GW2Emu.Tests/Test.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace GameRevision.GW2Emu.Tests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
